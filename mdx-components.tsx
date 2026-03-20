61e2031854e7c47ffed3eca93db8e2d85efb0d03
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-03-20 21:09:01 +0200

    Preliminary styling of the .mdx files.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,9 +6,35 @@ export function SampleComponent({ children }: { children: React.ReactNode }) {
 }
 
 const components: MDXComponents = {
-  h2: ({ children }) => <h2 className="text-red-500">{children}</h2>,
-  // eslint-disable-next-line jsx-a11y/alt-text
-  img: (props) => <Image height={400} width={400} {...(props as ImageProps)} />,
+  h1: ({ children }) => <h1 className="text-foreground">{children}</h1>,
+  h2: ({ children }) => <h2 className="text-secondary">{children}</h2>,
+  h3: ({ children }) => <h3 className="text-tertiary">{children}</h3>,
+  p: ({ children }) => <p className="text-foreground">{children}</p>,
+  ol: ({ children }) => (
+    <ol className="text-foreground [&_li::marker]:text-foreground">
+      {children}
+    </ol>
+  ),
+  ul: ({ children }) => (
+    <ul className="text-foreground [&_li::marker]:text-foreground">
+      {children}
+    </ul>
+  ),
+  img: ({ alt, ...props }) => (
+    <figure className="flex flex-col items-center">
+      <Image
+        height={400}
+        width={400}
+        alt={alt ?? ""}
+        {...(props as Omit<ImageProps, "alt">)}
+      />
+      {alt && (
+        <figcaption className="text-muted-foreground text-center text-sm">
+          {alt}
+        </figcaption>
+      )}
+    </figure>
+  ),
   MyComponent: SampleComponent,
 };
 
