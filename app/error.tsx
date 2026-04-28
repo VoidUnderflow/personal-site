63686446c846cae89b4af8576fdcb73f1e40bd74
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-28 12:39:03 +0300

    Use BackButton in error.tsx.

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { BackButton } from "@/components/error/BackButton";
 
 export default function Error() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 pt-16">
       <h1 className="font-logo text-9xl">500</h1>
       <h2 className="text-2xl">Something Went Wrong</h2>
-      <div>
-        <Button variant={"outline"} onClick={() => window.history.back()}>
-          Go Back
-        </Button>
-      </div>
+      <BackButton />
     </div>
   );
 }
