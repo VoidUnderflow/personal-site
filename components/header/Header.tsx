0bc05b2773f9a97d79d4376fd84349862fc7e4eb
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-05-01 15:10:18 +0300

    Remove bold because it was filling up the font.

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ className }: HeaderProps) {
       <nav className="flex flex-col gap-2 lg:flex-row lg:justify-between">
         <div className="flex flex-col items-center md:flex-row md:gap-8 lg:gap-4">
           <Link href="/">
-            <p className="font-logo text-tertiary hover:border-tertiary border-background border-2 px-1 text-2xl font-bold hover:border-dashed">
+            <p className="font-logo text-tertiary hover:border-tertiary border-background border-2 px-1 text-2xl hover:border-dashed">
               <span className="text-foreground">VOID</span>
               <span>UNDERFLOW</span>
             </p>
