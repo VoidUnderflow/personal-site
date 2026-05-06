0270e9663ff3ac25b964e22c3d263ed00cc7b7d8
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-05-07 00:25:06 +0300

    Add brushed navbar bg on click.

--- components/header/NavLink.tsx
+++ components/header/NavLink.tsx
@@ -6,13 +6,31 @@ interface NavLinkProps {
   children: React.ReactNode;
 }
 
+const svgFile = {
+  primary: "/navbar-button-1.svg",
+  secondary: "/navbar-button-2.svg",
+  tertiary: "/navbar-button-3.svg",
+  neutral: "/navbar-button-4.svg",
+};
+
 export default function NavLink({ href, color, children }: NavLinkProps) {
   return (
     <Link
       href={href}
-      className={`block border-2 border-dashed border-x-transparent border-t-transparent px-2 py-1 nav-link-${color} active:text-background text-foreground`}
+      className={`group relative block border-2 border-dashed border-x-transparent border-t-transparent border-b-${color} text-foreground px-2 py-1 hover:border-${color} active:text-background active:border-transparent`}
     >
-      {children}
+      <span
+        className={`pointer-events-none absolute inset-0 bg-${color} opacity-0 group-active:opacity-100`}
+        style={{
+          maskImage: `url('${svgFile[color]}')`,
+          maskSize: "cover",
+          maskPosition: "center",
+          WebkitMaskImage: `url('${svgFile[color]}')`,
+          WebkitMaskSize: "cover",
+          WebkitMaskPosition: "center",
+        }}
+      />
+      <span className="relative">{children}</span>
     </Link>
   );
 }
