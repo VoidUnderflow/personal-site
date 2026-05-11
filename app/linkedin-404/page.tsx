8b127fcad80b1e25317eb895dc80f2bfeea98481
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-05-11 12:35:32 +0300

    Improve LinkedIn 404 page.

--- app/linkedin-404/page.tsx
+++ app/linkedin-404/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import { BackButton } from "@/components/common/BackButton";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,14 +8,29 @@ export const metadata: Metadata = {
 
 export default function LinkedInComingSoon() {
   return (
-    <div>
-      <h1>LinkedIn - coming soon</h1>
-      <p>
-        Once I take a good photo of myself and understand how to frame my
-        employment gap on a website that honestly feels pretty hostile to such
-        things.
-      </p>
-      <Link href="/">Back to Home </Link>
+    <div className="flex h-full flex-1 flex-col items-center justify-center gap-8 pt-16">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="font-logo text-9xl">404</h1>
+        <p className="text-muted-foreground">Don&apos;t have a LinkedIn yet.</p>
+      </div>
+
+      <Card className="w-full max-w-xs">
+        <CardContent className="flex flex-col gap-2">
+          <p className="pb-1 text-sm font-bold">I&apos;ll make one...</p>
+          <div className="flex flex-col gap-1.5">
+            <div className="flex justify-between text-sm">
+              <span>soon(-ish)</span>
+              <span>possibly never</span>
+            </div>
+            <div className="flex h-1.5 w-full overflow-hidden rounded-full">
+              <div className="bg-tertiary h-full w-3/5" />
+              <div className="bg-secondary h-full w-2/5" />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+
+      <BackButton />
     </div>
   );
 }
